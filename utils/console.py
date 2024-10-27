ef965e6a058687296353b1080883fecb12f86a8b
Ben Tettmar <bentettmar@yahoo.com>
2024-10-27 02:24:47 +0000

    Added time to console outputs.

--- utils/console.py
+++ utils/console.py
@@ -1,4 +1,5 @@
 import colorama
+import datetime
 import os, sys, pystyle
 
 from . import config
@@ -14,6 +15,9 @@ def resize(columns, rows):
         os.system(f"mode con cols={columns} lines={rows}")
     else:
         os.system(f"echo '\033[8;{rows};{columns}t'")
+
+def get_formatted_time():
+    return datetime.datetime.now().strftime("%H:%M:%S")
 
 def print_banner():
     copyright_ = f"( Ghost v{config.VERSION} )"
@@ -47,19 +51,19 @@ def print_color(color, text):
     print(color + text + colorama.Style.RESET_ALL)
 
 def print_cmd(text):
-    print(f"{colorama.Fore.LIGHTBLUE_EX}{colorama.Style.BRIGHT}[COMMAND]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTBLUE_EX}{colorama.Style.BRIGHT}[COMMAND]{colorama.Style.RESET_ALL} {text}")
 
 def print_info(text):
-    print(f"{colorama.Fore.LIGHTGREEN_EX}{colorama.Style.BRIGHT}[INFO]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTGREEN_EX}{colorama.Style.BRIGHT}[INFO]{colorama.Style.RESET_ALL} {text}")
 
 def print_success(text):
-    print(f"{colorama.Fore.LIGHTGREEN_EX}{colorama.Style.BRIGHT}[SUCCESS]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTGREEN_EX}{colorama.Style.BRIGHT}[SUCCESS]{colorama.Style.RESET_ALL} {text}")
 
 def print_error(text):
-    print(f"{colorama.Fore.LIGHTRED_EX}{colorama.Style.BRIGHT}[ERROR]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTRED_EX}{colorama.Style.BRIGHT}[ERROR]{colorama.Style.RESET_ALL} {text}")
 
 def print_warning(text):
-    print(f"{colorama.Fore.LIGHTYELLOW_EX}{colorama.Style.BRIGHT}[WARNING]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTYELLOW_EX}{colorama.Style.BRIGHT}[WARNING]{colorama.Style.RESET_ALL} {text}")
 
 def print_cli(text):
-    print(f"{colorama.Fore.LIGHTMAGENTA_EX}{colorama.Style.BRIGHT}[CLI]{colorama.Style.RESET_ALL} {text}")
+    print(f"{colorama.Fore.LIGHTBLACK_EX}[{get_formatted_time()}] {colorama.Fore.LIGHTMAGENTA_EX}{colorama.Style.BRIGHT}[CLI]{colorama.Style.RESET_ALL} {text}")
